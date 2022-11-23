ebd925596838011a87a1e845ea1a27769dcf6f5c
Mark Foskett <markryanfoskett@gmail.com>
2022-11-23 20:51:43 +1100

    Added Front Page, Connected to Sanity CMS

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { ConnectWallet, useAddress, useDisconnect } from "@thirdweb-dev/react";
+import { GetServerSideProps } from 'next';
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
 
-type Props = {}
+interface Props {
+  collection: Collection,
+}
 
-function NFTDropPage({}: Props) {
+function NFTDropPage({collection}: Props) {
 
   // Authentication
   const address = useAddress();
@@ -15,11 +21,11 @@ function NFTDropPage({}: Props) {
       <div className='lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500'>
         <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen '>
           <div className='bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl'>
-            <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72 ' src="https://links.papareact.com/8sg" alt=""  />
+            <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72 ' src={urlFor(collection.previewImage).url()} alt=""  />
           </div>
           <div className='text-center p-5 space-y-2'>
-            <h1 className='text-4xl font-bold text-white'>BOBSON APES</h1>
-            <h2 className='text-xl text-gray-300'>A collection of the top 1% of Apes that will one day overthrow humanity!</h2>
+            <h1 className='text-4xl font-bold text-white'>{collection.nftCollectionName}</h1>
+            <h2 className='text-xl text-gray-300'>{collection.description}</h2>
           </div>
         </div>
       </div>
@@ -27,14 +33,16 @@ function NFTDropPage({}: Props) {
       <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
         {/* Header */}
         <header className='flex itemse-center justify-between'>
-          <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>The{' '}<span className='font-extrabold underline decoration-pink-600/50'>BOBSON</span>{' '}NFT Grand Exchange!</h1>
+          <Link href={`/`}>
+            <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>The{' '}<span className='font-extrabold underline decoration-pink-600/50'>BOBSON</span>{' '}NFT Grand Exchange!</h1>
+          </Link>
           <ConnectWallet accentColor="#e11d48" colorMode='light'/>
         </header>
         <hr className='my-2 border'/>
         {/* Content */}
         <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
-          <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="" />
-          <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>The BOBSON Ape Exclusive Club | NFT Drop</h1>
+          <img className='w-80 object-cover pb-10 lg:h-40' src={urlFor(collection.mainImage).url()} alt="" />
+          <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>{collection.title}</h1>
           <p className='text-green-500 pt-2 text-xl'>7 / 21 NFTs claimed!</p>
         </div>
         {/* Mint Button */}
@@ -50,3 +58,47 @@ function NFTDropPage({}: Props) {
 
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+  const query =
+  `*[_type == "collection" && slug.current == $id][0]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+        asset
+    },
+    previewImage {
+        asset
+    },
+    slug {
+        current
+    },
+    creator-> {
+        _id,
+        name,
+        address,
+        slug {
+            current
+        },
+    },
+  }`
+
+  const collection = await sanityClient.fetch(query, {
+    id: params?.id,
+  })
+
+  if (!collection){
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      collection,
+    }
+  }
+}
